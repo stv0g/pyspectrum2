5c61892f1980fcfc8e275b72e5e1ceebca5cb481
Steffen Vogel <post@steffenvogel.de>
2019-04-25 21:46:26 +0200

    init commit
    Source code has been separated from transWhat repo

---
+++ README.rst
@@ -0,0 +1,32 @@
+pyspectrum2
+===========
+
+pyspectrum2 implements the Protobuf-based interface to Spectrum2 required for building Python-based Spectrum2 backends.
+
+Support
+-------
+
+For support and discussions please join the XMPP MUC: **spectrum@conference.spectrum.im**.
+
+Example
+-------
+
+For an exampe backend which uses pyspectrum2 see transWhat_.
+
+Contributors
+------------
+
+-  @stv0g (Steffen Vogel)
+-  @sarangbh
+
+License
+-------
+
+pyspectrum2 is licensed under the GPLv3_ license.
+
+Links
+-----
+
+.. _Spectrum 2: http://www.spectrum.im
+.. _GPLv3: COPYING.rst
+.. _transWhat: https://github.com/stv0g/transwhat/
---
+++ Spectrum2/iochannel.py
@@ -0,0 +1,43 @@
+import asyncore, socket
+import logging
+import sys
+
+class IOChannel(asyncore.dispatcher):
+	def __init__(self, host, port, callback, closeCallback):
+		asyncore.dispatcher.__init__(self)
+
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connect((host, port))
+		self.logger = logging.getLogger(self.__class__.__name__)
+
+		self.callback = callback
+		self.closeCallback = closeCallback
+		self.buffer = bytes("")
+
+	def sendData(self, data):
+		self.buffer += data
+
+	def handle_connect(self):
+		pass
+
+	def handle_close(self):
+		self.close()
+
+	def handle_read(self):
+		data = self.recv(65536)
+		self.callback(data)
+
+	def handle_write(self):
+		sent = self.send(self.buffer)
+		self.buffer = self.buffer[sent:]
+
+	def handle_close(self):
+		self.logger.info('Connection to backend closed, terminating.')
+		self.close()
+		self.closeCallback()
+
+	def writable(self):
+		return (len(self.buffer) > 0)
+
+	def readable(self):
+		return True
